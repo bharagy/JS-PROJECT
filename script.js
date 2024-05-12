16297e700c484107359b94bc11304c297fa0c3d1
bharagy <145537623+bharagy@users.noreply.github.com>
2024-05-12 15:04:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function change1(){
+    document.getElementById('pic').src='bmw1-removebg-preview.png';
+}
+function change2(){
+    document.getElementById('pic').src='bmw2-removebg-preview.png';
+}
+function change3(){
+    document.getElementById('pic').src='bmw3-removebg-preview.png';
+}
+
+function change4(){
+    document.getElementById('pic').src='bmw4-removebg-preview.png';
+}
